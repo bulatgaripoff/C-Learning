5663a13c7f550da5fc68485b8a32260ccefdcce3
Bulat Garipov <garipovbi@yandex.ru>
2022-08-03 00:09:43 +0300

    Модуль 8. Телефонная книга. Бета-версия

---
+++ 08 August/PhoneBook/PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    // Что нужно сделать
+    // Пользователю итеративно предлагается вводить в программу номера телефонов и ФИО их владельцев.
+    // В процессе ввода информация размещается в Dictionary, где ключом является номер телефона,
+    // а значением — ФИО владельца.Таким образом, у одного владельца может быть несколько номеров телефонов.
+    // Признаком того, что пользователь закончил вводить номера телефонов, является ввод пустой строки. 
+    // Далее программа предлагает найти владельца по введенному номеру телефона.Пользователь вводит номер
+    // телефона и ему выдаётся ФИО владельца.Если владельца по такому номеру телефона не зарегистрировано,
+    // программа выводит на экран соответствующее сообщение.
+
+
+    // Совет
+    // Для того, чтобы находить значение в Dictionary, нужно использовать TryGetValue.
+
+
+
+    // Что оценивается
+    // Программа разделена на логические методы.
+    // Для хранения элементов записной книжки используется Dictionary.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> PhoneBook = new Dictionary<string, string>();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1-Ввести запись; 2-Поиск абонента");
+                
+                string mode = Console.ReadLine();
+                switch (mode)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Введите номер абонента:");
+                        string key = Console.ReadLine();
+                        Console.WriteLine("Введите ФИО абонента:");
+                        string value = Console.ReadLine();
+                        PhoneBook.Add(key, value);
+
+                        int menu2 = 1;
+
+                        do
+                        {
+                            Console.WriteLine("1 - Добавить новую запись");
+                            Console.WriteLine("2 - Выход в главное меню");
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    Console.WriteLine("Введите номер абонента:");
+                                    string new_key = Console.ReadLine();
+                                    Console.WriteLine("Введите ФИО абонента:");
+                                    string new_value = Console.ReadLine();
+                                    PhoneBook.Add(new_key, new_value);
+                                    break;
+                                case "2":
+                                    menu2=2;
+                                    break;
+                            }
+                        }
+                        while (menu2!=2);
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine("Введите номер абонента:");
+                        string search_key = Console.ReadLine();
+                        var exist_number = PhoneBook.ContainsKey(search_key);
+                        if (exist_number==true)
+                        {
+                            Console.WriteLine(PhoneBook[search_key]);
+                        }
+                        else Console.WriteLine("Такой номер отсутствует!");
+
+                        int menu3 = 1;
+
+                        do
+                        {
+                            Console.WriteLine("1 - Поиск нового абонента");
+                            Console.WriteLine("2 - Выход в главное меню");
+                            switch (Console.ReadLine())
+                            {
+                                case "1":
+                                    Console.WriteLine("Введите номер абонента:");
+                                    string new_search_key = Console.ReadLine();
+                                    var new_exist_number = PhoneBook.ContainsKey(search_key);
+                                    if (new_exist_number==true)
+                                    {
+                                        Console.WriteLine(PhoneBook[new_search_key]);
+                                    }
+                                    else Console.WriteLine("Такой номер отсутствует!");
+                                    break;
+                                case "2":
+                                    menu3=2;
+                                    break;
+                            }
+                        }
+                        while (menu3!=2);
+
+                        break;
+
+                }
+
+            }    
+
+        }
+    }
+}
