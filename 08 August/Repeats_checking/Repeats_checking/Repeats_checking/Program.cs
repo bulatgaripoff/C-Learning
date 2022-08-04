9c2de3b71d5b92af9a91cd44b5820e2ee83dca5b
Bulat Garipov <garipovbi@yandex.ru>
2022-08-04 21:50:08 +0300

    Работа с HashSet

---
+++ 08 August/Repeats_checking/Repeats_checking/Repeats_checking/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+// Что нужно сделать
+// Пользователь вводит число. Число сохраняется в HashSet коллекцию.
+// Если такое число уже присутствует в коллекции, то пользователю на экран
+// выводится сообщение, что число уже вводилось ранее. Если числа нет, то
+// появляется сообщение о том, что число успешно сохранено. 
+
+// Советы и рекомендации
+// Для добавление числа в HashSet используйте метод Add. 
+
+// Что оценивается
+// В программе в качестве коллекции используется HashSet.
+
+namespace Repeats_checking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int menu = 1;
+            string path = @"numbers.txt";
+
+            do
+            {
+                Console.WriteLine("1 - просмотр чисел");
+                Console.WriteLine("2 - добавить число");
+                Console.WriteLine("3 - выход");
+
+                HashSet<int> Numbers_collection = new HashSet<int>();
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.Clear();
+                        using (StreamReader ContentStream = new StreamReader(path))
+                        {
+                            while (!ContentStream.EndOfStream)
+                            {
+                                string[] Numbers_string = ContentStream.ReadLine().Split(',');
+                                int[] Numbers = new int[Numbers_string.Length];
+                                for (int i = 0; i < Numbers_string.Length; i++)
+                                {
+                                    Numbers[i]=int.Parse(Numbers_string[i]);
+                                    Numbers_collection.Add(int.Parse(Numbers_string[i]));
+                                }
+                            }
+                            foreach (int num in Numbers_collection)
+                            {
+                                Console.Write($"{num} ");
+                            }
+
+                            Console.WriteLine();
+                        }
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Введите число:");
+                        int.TryParse(Console.ReadLine(), out int Number);
+                        int number = Number;
+                        if (Numbers_collection.Contains(number)==true)
+                            Console.WriteLine("Коллекция уже содержит данное число!");
+                        else
+                        {
+                            Numbers_collection.Add(number);
+                            Console.WriteLine("Число успешно добавлено в коллекцию!");
+                        }
+
+                        using (StreamWriter WritingStream = new StreamWriter(path, true, Encoding.UTF8))
+                        {
+                            foreach (int num in Numbers_collection)
+                            {
+                                WritingStream.Write($",{num}");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        menu=0;
+                        break;
+                }
+            } while (menu!=0);
+        }
+    }
+}
---
+++ 08 August/Repeats_checking/Repeats_checking/Repeats_checking/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Repeats_checking")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Repeats_checking")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d076ba19-f67b-4f2f-bf92-41864a08b88d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
